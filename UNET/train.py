cbd96140cd048579350330b47b435c817981fdb7
yenchunlu <940058@gmail.com>
2023-05-22 17:36:52 -0400

    train.py add metrics calculation

--- UNET/train.py
+++ UNET/train.py
@@ -1,16 +1,41 @@
 
 import os
 import time
+from operator import add
 from glob import glob
 
 import torch
 from torch.utils.data import DataLoader
 import torch.nn as nn
+from sklearn.metrics import accuracy_score, f1_score, jaccard_score, precision_score, recall_score
+
 
 from data import DriveDataset
 from model import build_unet
 from loss import DiceLoss, DiceBCELoss
 from utils import seeding, create_dir, epoch_time
+import numpy as np
+
+def calculate_metrics(y_true, y_pred):
+    """ Ground truth """
+    y_true = y_true.cpu().detach().numpy()
+    y_true = y_true > 0.5
+    y_true = y_true.astype(np.uint8)
+    y_true = y_true.reshape(-1)
+
+    """ Prediction """
+    y_pred = y_pred.cpu().detach().numpy()
+    y_pred = y_pred > 0.5
+    y_pred = y_pred.astype(np.uint8)
+    y_pred = y_pred.reshape(-1)
+
+    score_jaccard = jaccard_score(y_true, y_pred)
+    score_f1 = f1_score(y_true, y_pred)
+    score_recall = recall_score(y_true, y_pred)
+    score_precision = precision_score(y_true, y_pred)
+    score_acc = accuracy_score(y_true, y_pred)
+
+    return [score_jaccard, score_f1, score_recall, score_precision, score_acc]
 
 def train(model, loader, optimizer, loss_fn, device):
     epoch_loss = 0.0
@@ -22,13 +47,17 @@ def train(model, loader, optimizer, loss_fn, device):
 
         optimizer.zero_grad()
         y_pred = model(x)
+        score = calculate_metrics(y, y_pred)
+        metrics_score = [0.0, 0.0, 0.0, 0.0, 0.0]
+        metrics_score = list(map(add, metrics_score, score))
+
         loss = loss_fn(y_pred, y)
         loss.backward()
         optimizer.step()
         epoch_loss += loss.item()
 
     epoch_loss = epoch_loss/len(loader)
-    return epoch_loss
+    return epoch_loss, metrics_score
 
 def evaluate(model, loader, loss_fn, device):
     epoch_loss = 0.0
@@ -92,7 +121,7 @@ if __name__ == "__main__":
         num_workers=2
     )
 
-    device = torch.device('cpu')   ## cpu
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')   ## use GPU if available, else use CPU
     model = build_unet()
     model = model.to(device)
 
@@ -103,10 +132,12 @@ if __name__ == "__main__":
     """ Training the model """
     best_valid_loss = float("inf")
 
+    metrics_score = [0.0, 0.0, 0.0, 0.0, 0.0]
+
     for epoch in range(num_epochs):
         start_time = time.time()
 
-        train_loss = train(model, train_loader, optimizer, loss_fn, device)
+        train_loss, metric_score = train(model, train_loader, optimizer, loss_fn, device)
         valid_loss = evaluate(model, valid_loader, loss_fn, device)
 
         """ Saving the model """
@@ -117,10 +148,18 @@ if __name__ == "__main__":
             best_valid_loss = valid_loss
             torch.save(model.state_dict(), checkpoint_path)
 
+        jaccard = metric_score[0]/len(train_X)
+        f1 = metric_score[1]/len(train_X)
+        recall = metric_score[2]/len(train_X)
+        precision = metric_score[3]/len(train_X)
+        acc = metric_score[4]/len(train_X)
+
+
         end_time = time.time()
         epoch_mins, epoch_secs = epoch_time(start_time, end_time)
 
         data_str = f'Epoch: {epoch+1:02} | Epoch Time: {epoch_mins}m {epoch_secs}s\n'
         data_str += f'\tTrain Loss: {train_loss:.3f}\n'
+        data_str += f'\tTrain:\n\tJaccard: {jaccard:1.4f} - F1: {f1:1.4f} - Recall: {recall:1.4f} - Precision: {precision:1.4f} - Acc: {acc:1.4f}\n'
         data_str += f'\t Val. Loss: {valid_loss:.3f}\n'
         print(data_str)
